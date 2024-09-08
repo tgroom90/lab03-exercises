377a37fd3914842557b452a3d310aebf1088ffbb
Travis Groom <tgroom@arden.cs.unca.edu>
2024-09-08 19:44:06 -0400

    Added python folder and find_duplicates.py script.

---
+++ python/find_duplicates.py
@@ -0,0 +1,22 @@
+def find_duplicates_nested_loop(l: list) -> list:
+    unique = set()
+    results = set()
+
+    for num in l:
+        if num in unique:
+            results.add(num)
+        else:
+            unique.add(num)
+
+    return results
+
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_nested_loop(sample1))
+    print("Sample 2:", find_duplicates_nested_loop(sample2))
+    print("Sample 3:", find_duplicates_nested_loop(sample3))
+    print("Sample 4:", find_duplicates_nested_loop(sample4))
