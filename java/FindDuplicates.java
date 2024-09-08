0ec2189fb630b688dcce208b477d551b223c08b1
Travis Groom <tgroom@arden.cs.unca.edu>
2024-09-08 19:07:28 -0400

    Added FindDuplicates.java script

---
+++ java/FindDuplicates.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDuplicates {
+
+    public static List<Integer> findModeNestedLoops(List<Integer> l) {
+        List<Integer> unique = new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < l.size(); i++) {
+            if (unique.contains(l.get(i))) {
+                if (result.contains(l.get(i)) == false) {
+                    result.add(l.get(i));
+                }
+            }
+            unique.add(l.get(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> sample1 = new ArrayList<Integer>(Arrays.asList(3, 7, 5, 6, 7, 4, 8, 5, 7, 66));
+        List<Integer> sample2 = new ArrayList<Integer>(Arrays.asList(3, 5, 6, 4, 4, 5, 66, 6, 7, 6));
+        List<Integer> sample3 = new ArrayList<Integer>(Arrays.asList(3, 0, 5, 1, 0));
+        List<Integer> sample4 = new ArrayList<Integer>(Arrays.asList(3));
+        System.out.println("Sample 1: " + findModeNestedLoops(sample1));
+        System.out.println("Sample 2: " + findModeNestedLoops(sample2));
+        System.out.println("Sample 3: " + findModeNestedLoops(sample3));
+        System.out.println("Sample 4: " + findModeNestedLoops(sample4));
+    }
+
+}
